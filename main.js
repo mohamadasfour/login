1a388dea83c33d4c265fb8b53d797e18c82557ff
MohamadAsfour <mohamad.asfour.0001@gmail.com>
2019-04-24 08:46:09 +0300

    simple login form

---
+++ main.js
@@ -0,0 +1,25 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please Enter all fields';
+        console.log('btn is clicked');
+        setTimeout(() => msg.remove(), 3000);
+    }else {
+        console.log('success');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+        // clear entered info from fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
